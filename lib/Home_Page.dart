0dd0055bd2c3d1e7d4d612fd38f99b91da6948e0
Bunty Kumar <bk2271999@gmail.com>
2022-08-10 16:03:36 +0530

    first commit..

---
+++ lib/Home_Page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const int days = 45;
+    const String name = "silent coder";
+    return Scaffold(
+      appBar: AppBar(title:Text("My App")),
+      body: Center(
+        child: Text("Welcome to $days days of flutter by $name"),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application2/Home_Page.dart';
 
 void main() {
   runApp(myApp());
 }
 
 class myApp extends StatelessWidget {
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
 }
